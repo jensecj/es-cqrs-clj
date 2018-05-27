fe51401b4f03c569d0c3e552e99519b99a1c4ab3
Jens Christian Jensen <jensecj@gmail.com>
2018-05-27 11:12:47 +0200

    updated tag line

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject es-cqrs-clj "0.1.0-SNAPSHOT"
-  :description "simple pure clojure event sourcing, an educational example"
+  :description "An example of event sourcing + CQRS in pure clojure"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot es-cqrs-clj.core)
