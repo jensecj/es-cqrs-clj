bde30e30893b4a187aa6c0ff88d22ee0bb1140b3
Jens Christian Jensen <jensecj@gmail.com>
2018-05-30 15:50:25 +0200

    don't try to be smart, just store events in a sequence.

--- src/es_cqrs_clj/inmemory_eventlog.clj
+++ src/es_cqrs_clj/inmemory_eventlog.clj
@@ -4,17 +4,8 @@
 (def^{:private true} store (atom {}))
 (reset! store {})
 
-;;(swap! store assoc-in [:mytopic] [])
-;;(conj '[1 2 3] 'e)
-;;(swap! store update-in [:sometopic] (fn [s] (conj s {:name "3"})))
-
 (defn- handle-add-event [topic event]
   (println (format "log: adding event to log: %s" event))
-
-  (if (empty? (topic @store))
-    (swap! store assoc-in [topic] []))
-
-  ;; store the event in the log
   (swap! store update-in [topic] conj event))
 
 (defn- handle-get-events [topic]
