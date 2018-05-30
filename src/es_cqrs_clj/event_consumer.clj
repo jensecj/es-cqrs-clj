8c8db327135daeff4bc77b07543790116b807082
Jens Christian Jensen <jensecj@gmail.com>
2018-05-30 15:54:54 +0200

    implement some of event-consumers function using sequences

--- src/es_cqrs_clj/event_consumer.clj
+++ src/es_cqrs_clj/event_consumer.clj
@@ -10,17 +10,24 @@
 
 (defn do-get-before-id [eventlog topic event-id])
 (defn do-get-after-id [eventlog topic event-id])
-(defn do-get-before [eventlog topic timestamp])
+
+(defn do-get-before [eventlog topic timestamp]
+  (drop-while #(> (:timestamp %) timestamp) (elp/get-events eventlog topic)))
+
 (defn do-get-after [eventlog topic timestamp]
-  (filter #(> (:timestamp %) timestamp) (elp/get-events eventlog topic)))
-(defn do-get-between [eventlog topic start-timestamp end-timestamp])
+  (take-while #(> (:timestamp %) timestamp) (elp/get-events eventlog topic)))
+
+(defn do-get-between [eventlog topic start-timestamp end-timestamp]
+  (->> (elp/get-events eventlog topic)
+       (drop-while #(>= (:timestamp %) end-timestamp))
+       (take-while #(> (:timestamp %) start-timestamp))))
 
 (defprotocol EventConsumerProtocol
   (get-before-id [this topic event-id] "Get all events that have been committed before event with EVENT-ID")
   (get-after-id [this topic event-id] "Get all events that have been committed after event with EVENT-ID")
   (get-before [this topic timestamp] "Get all events that have been committed before TIMESTAMP")
   (get-after [this topic timestamp] "Get all events that have been committed after TIMESTAMP")
-  (get-between [this topic start-timestamp end-timestamp]) "Get all events that have been committed between START-TIMESTAMP and END-TIMESTAMP")
+  (get-between [this topic start-timestamp end-timestamp] "Get all events that have been committed between START-TIMESTAMP and END-TIMESTAMP"))
 
 (defrecord EventConsumer [eventlog]
   EventConsumerProtocol
